87446ab6c3e4324915f8f71816dbc4692d6ad6a1
Peu <91740410+PedroEmanuelMoreiraCarvalho@users.noreply.github.com>
2024-11-09 07:50:31 -0300

    Books transmition implemented

--- peer.py
+++ peer.py
@@ -1,23 +1,20 @@
 import socket
 import os
+import time
 from threading import Thread
 
 PORT = 50001        
 
 class Peer():
     def __init__(self):
-        self.capacity = 7
-        self.peers_hash_table = []
-        
-        for i in range(self.capacity):
-            self.peers_hash_table.append([])
-        
         self.peers = []
         self.server = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
         self.running = True
         self.listen(PORT)
         
         self.threads = []
+        
+        self.waiting_book = False
     
     def listen(self,port):
         ip = self.getIp()
@@ -37,7 +34,6 @@ class Peer():
             
             self.threads.append(listen_thread)
             self.peers.append(cli)
-            self.addAddressToHashTable(cli)
             print("nova conexão")
         
     def off(self):
@@ -48,14 +44,12 @@ class Peer():
         self.server.close()
     
     def listenPeer(self,peer):
-        print("ouvinte")
+        print("ouvindo")
         while self.running:
             try:
                 data = peer.recv(1024)
                 if data:
-                    # print(f"from conn{peer.getpeername()}:",data.decode())
-                    
-                    received_message = data.decode().split(":2:")
+                    received_message = data.decode().split(":2:",1)
                     self.analyzeMessage(received_message)
                     
                     if(received_message[0] == "turning_off"):
@@ -72,16 +66,7 @@ class Peer():
         peer.close()
         
     def removePeer(self, peer_to_remove):
-        key = self.getHashKey(peer_to_remove.getpeername()[0])
         self.peers.remove(peer_to_remove)
-        self.peers_hash_table[key].remove(peer_to_remove)
-    
-    def addAddressToHashTable(self,peer):
-        key = self.getHashKey(peer.getpeername()[0])
-        self.peers_hash_table[key].append(peer)
-    
-    def getHashKey(self,address):
-        return int(address.split('.')[-1]) % self.capacity
     
     def getIp(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -93,42 +78,7 @@ class Peer():
         finally:
             s.close()
         return IP
-    
-    def checkExistence(self,address, visiteds):
-        visiteds.append(self.getIp())
         
-        key = self.getHashKey(address)
-        
-        # checando se endereço está na lista de conexões
-        for peer in self.peers_hash_table[key]:
-            if(peer.getpeername()[0] == address):
-                print("encontrado")
-                return peer
-            
-        for peer in self.peers:
-            #verifica se par já foi visitado
-            if peer.getpeername()[0] in visiteds:
-                continue
-            
-            visiteds_str = ""
-            
-            for visited in visiteds:
-                visiteds_str += visited + ","
-            
-            # print("visitados:",visiteds_str)
-            content = address + "::" + visiteds_str
-            
-            message_send = {
-                "type": "search",
-                "content": content
-            }
-            
-            searching_str = self.encodeString(message_send)
-            peer.send(searching_str.encode())
-            
-        print("não encontrado")
-        
-    
     def emit(self,message, mode):
         message_send = {
             "type": mode,
@@ -162,23 +112,16 @@ class Peer():
         content = received_message[1]
         
         if mode == "send":
-            print(content[1:-1]) 
-        elif mode == "search":
-            info = content[1:-1].split("::") 
-            
-            address = info[0]
-            visiteds = info[1].split(",")
-
-            self.checkExistence(address,visiteds)
+            print(content[1:-1])
         elif mode == "searching_book":
-            info = content[1:-1].split("::")
+            info = content[1:-1].split("::",1)
             
             searched_book = info[0]
             visiteds = info[1]
             
             self.checkPathToBook(searched_book,visiteds)
         elif mode == "send_back":
-            info = content[1:-1].split("::3::")
+            info = content[1:-1].split("::3::",1)
             
             path = info[0]
             book = info[1]
@@ -186,11 +129,13 @@ class Peer():
             self.sendBookBack(path,book)
     
     def connect(self,host,port):
-        conn = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-        conn.connect((host,port))
-        Thread(target=self.listenPeer,args=(conn,)).start()
-        self.peers.append(conn)
-        self.addAddressToHashTable(conn)
+        try:
+            conn = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
+            conn.connect((host,port))
+            Thread(target=self.listenPeer,args=(conn,)).start()
+            self.peers.append(conn)
+        except Exception as e:
+            print("Algo deu errado na conexão:",e)
         
     def getBooks(self):
         path = ".//books"
@@ -200,16 +145,33 @@ class Peer():
         except Exception as e:
             return []
     
+    def createBook(self,title,content):
+        try:
+            book_file_dir = ".//books//" + title + ".txt"
+                
+            f = open(book_file_dir, "w")
+            f.write(content)
+            f.close()
+        except Exception as e:
+            print("erro na criação de livro:",e)
+    
     def sendBookBack(self,path,book):
         final_path = path.split(",")
         final_path.pop()
         if(len(final_path) == 0):
             try:
-                f = open(".//books//livro_final.txt", "w")
-                f.write(book)
+                content = book.split("::",1)
+
+                titulo = content[0]
+                book_text = content[1]
+                
+                book_file_dir = ".//books//" + titulo
+                
+                f = open(book_file_dir, "w")
+                f.write(book_text)
                 f.close()
                 
-                print("livro salvo")
+                self.waiting_book = False
             except Exception:
                 print("algo deu errado")
             return
@@ -221,7 +183,6 @@ class Peer():
         message = new_path[0:-1] + "::3::" + book
             
         self.emitTo(mode = "send_back", message = message, address = final_path[-1])
-        
     
     def checkPathToBook(self,book_searched,previous_path):
         my_ip = self.getIp()
@@ -231,8 +192,7 @@ class Peer():
         
         previous_path += str(my_ip) + ','
         
-        books_getted = self.getBooks()
-        if book_searched in books_getted:
+        if self.checkBook(book_searched):
             book_coded = self.encodeBook(book_searched)
             self.sendBookBack(previous_path[0:-1],book_coded)
             return
@@ -244,8 +204,11 @@ class Peer():
         try:
             book_path = ".//books//" + book_name
             book_file = open(book_path, "r")
-        
-            return book_file.read()
+            book = book_file.read()
+            
+            book = book_name +"::"+ book
+            
+            return book 
         except Exception as e:
             print("algo deu errado:",e)
                 
@@ -258,6 +221,28 @@ class Peer():
         message = book_searched + "::" + final_path
         return message
         
+    def waitBook(self):
+        self.waiting_book = True
+        count = 0
+        
+        while self.waiting_book:
+            if count % 10 == 0:
+                print("   esperando livro...")
+            count += 1
+            time.sleep(0.1)
+            if count == 30:
+                print("livro não encontrado")
+                return
+        
+        print("livro encontrado")
+        
+    def checkBook(self,book_searched):
+        books_getted = self.getBooks()
+        if book_searched in books_getted:
+            return True
+        return False
+    
+    
 if __name__ == "__main__":
     if not os.path.exists(".//books"):
         os.makedirs(".//books")
@@ -266,24 +251,35 @@ if __name__ == "__main__":
     print(peer.getIp())
     
     while True:
-        option = input("digite a opção:\n1-conectar\n2-procurar livro\n3-checar endereço\n4-livros possuídos\n5-sair\n>")
-        if option == "1":
-            ip = input("Digite o ip:")
-            port = PORT
-            peer.connect(ip,port)
-        elif option == "2":
-            book = input("digite o nome do livro:")
-            peer.checkPathToBook(book,'')
-        elif option == "3":
-            address = input("digite o endereço:")
-            peer.checkExistence(address,[])
-        elif option == "4":
-            books = peer.getBooks()
-            print("livros possuídos:")
-            for book in books:
-                print("   -",book)
-        elif option == "5":
-            peer.off()
-            exit()
-        else:
-            print("opção inválida")
+        try:
+            print("====================================================")
+            option = input("digite a opção:\n1-conectar\n2-procurar livro\n3-escrever livro\n4-livros possuídos\n5-sair\n>").strip()
+            if option == "1":
+                ip = input("Digite o ip:")
+                port = PORT
+                peer.connect(ip,port)
+            elif option == "2":
+                book = input("digite o nome do livro:")
+                if peer.checkBook(book):
+                    print("Livro já obtivo")
+                else:
+                    peer.checkPathToBook(book,'')
+                    peer.waitBook()
+            elif option == "3":
+                title = input("digite o título:")
+                content = input("digite o conteúdo:")
+                peer.createBook(title,content)
+            elif option == "4":
+                books = peer.getBooks()
+                print("livros possuídos:")
+                if len(books) == 0:
+                    print("   nenhum livro possuído")
+                for book in books:
+                    print("   -",book)
+            elif option == "5":
+                peer.off()
+                exit()
+            else:
+                print("opção inválida")
+        except Exception:
+            print("algo deu errado...")
